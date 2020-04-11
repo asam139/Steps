1743a3a7da82c96507140b0196cedfede992647d
Saul Moreno Abril <93sauu@gmail.com>
2020-04-11 17:50:57 +0200

    Clear code

--- Sources/Steps/Steps.swift
+++ Sources/Steps/Steps.swift
@@ -1,24 +1,6 @@
 import SwiftUI
 import Combine
 
-public class StepsConfig: ObservableObject {
-    public var spacing: CGFloat = 5
-    public var size: CGFloat = 14
-    public var lineThickness: CGFloat = 2
-
-    public var primaryColor: Color = Color.blue
-    public var secondaryColor: Color = Color.white
-    public var disabledColor: Color = Color.gray
-
-    #if os(iOS)
-    public var image: Image? = Image(systemName: "checkmark")
-    #endif
-
-    #if os(macOS)
-    public var image: Image? = nil
-    #endif
-}
-
 public struct Steps: View {
 
     var config: StepsConfig = StepsConfig()
