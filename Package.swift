33b4e6b4a17215715c48603ac836c94fb3443ade
Saul Moreno Abril <93sauu@gmail.com>
2020-04-19 18:24:27 +0200

    Fixing workflow

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/asam139/SwifterSwiftUI.git", from: "0.2.0"),
+        .package(url: "https://github.com/asam139/SwifterSwiftUI.git", from: "0.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
