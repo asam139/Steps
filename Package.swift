1545b24ba344139261c11067bba5beaf8265fc12
Saul Moreno Abril <93sauu@gmail.com>
2020-06-10 19:12:49 +0200

    update deps

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/asam139/SwifterSwiftUI.git", from: "0.2.1"),
-        .package(url: "https://github.com/nalexn/ViewInspector", from: "0.3.11"),
+        .package(url: "https://github.com/asam139/SwifterSwiftUI.git", .upToNextMajor(from: "0.4.0")),
+        .package(url: "https://github.com/nalexn/ViewInspector", .upToNextMajor(from: "0.3.12")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
